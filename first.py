c93c55db44b3ebc2507bae8426667e468347da05
Blessed-K <kimaniblessed545@gmail.com>
2024-02-27 21:00:33 +0300

    first python

---
+++ first.py
@@ -0,0 +1,11 @@
+# Asking the user for their name and storing it in a variable called "name"
+name = input("What is your name? ")
+
+# Asking the user for their age and storing it in a variable called "age"
+age = input("How old are you? ")
+
+# Asking the user for their location and storing it in a variable called "location"
+location = input("Where do you live? ")
+
+# Printing out a personalized message using the user's name, age, and location
+print("Hello {}, you are {} years old and live in {}.".format(name, age, location))
